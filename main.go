a24703cdd42499bd0db4461343c177578b481b98
Alili <alirezatebyanian80@gmail.com>
2025-10-05 16:16:31 +0330

    armstrong_number check program

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func main() {
+	var input int
+	fmt.Print("enter a number: ")
+	fmt.Scanln(&input)
+
+	str_input := strconv.Itoa(input)
+	tavan := len(str_input)
+
+	final_sum := 0
+
+	for _, digit := range str_input {
+
+		int_digit, err := strconv.Atoi(string(digit))
+		if err != nil {
+			// ... handle error
+			panic(err)
+		}
+
+		each_digit_powered := math.Pow(float64(int_digit), float64(tavan))
+		final_sum = final_sum + int(each_digit_powered)
+
+	}
+	if input == final_sum {
+		fmt.Println("it is armstrong")
+	} else {
+		fmt.Println("it is not armstrong")
+	}
+
+}
